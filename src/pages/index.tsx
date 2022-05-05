2fa6158061acc41b39818856322a2a1da146ffab
Suelio Santos <sueliosantos@gmail.com>
2022-05-04 22:08:03 -0300

    Iniciando o projeto

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div
+      className={` 
+    bg-gradient-to-r 
+    from-green-500
+    to-blue-500
+    text-3xl
+    `}
+    >
+      <h1>ADMIN</h1>
+    </div>
+  );
+}
